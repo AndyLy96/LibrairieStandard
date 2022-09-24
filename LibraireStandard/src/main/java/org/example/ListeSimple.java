7d032bc93b50caa9f0f4a8ec823d8c8c713792f0
f <2157338@cegepmontpetit.ca>
2022-09-24 18:46:49 -0400

    FCT ListeSImple

--- LibraireStandard/src/main/java/org/example/ListeSimple.java
+++ LibraireStandard/src/main/java/org/example/ListeSimple.java
@@ -6,18 +6,22 @@ import java.util.List;
 
 public class ListeSimple {
     public static void main( String[] args ) throws IOException {
-        repete(4,2);
+        repete(4,4);
     }
 
     public static void repete(int n, int nombreFois) {
         List<Integer> liste = new ArrayList<Integer>();
-        for (int i = 0; i < n; i++) {
-            for (int o=0; o<nombreFois;o++)
+        int ger = 1;
+        for (int p = 0; p < n; p++)
+        {
+            for (int i = 0; i < nombreFois; i++ )
             {
-                System.out.print(1);
+                liste.add(ger);
             }
-
+            ger++;
         }
+        System.out.println(liste);
+
 
     }
 }
