537487f73bf788ff781f8c7d2996a70cc8e4f6fd
f <2157338@cegepmontpetit.ca>
2022-10-03 10:12:48 -0400

    FCT EntrezUnNombre FIX

--- LibraireStandard/src/main/java/org/example/EntrezUnNombre.java
+++ LibraireStandard/src/main/java/org/example/EntrezUnNombre.java
@@ -1,2 +1,18 @@
-package org.example;public class EntrezUnNombre {
+package org.example;
+
+import java.util.Scanner;
+
+public class EntrezUnNombre {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nombre");
+        while (scanner.hasNext() && !scanner.hasNextInt())
+        {
+            System.out.println("Nombre pls");
+            scanner.next();
+        }
+
+        int nb = scanner.nextInt();
+        System.out.println("votre nombre est " + nb);
+    }
 }
